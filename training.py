21dbcff714c27682329104d598eebb4f91a34daf
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2023-11-06 16:12:13 +0300

    Update training.py

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 from fastai.vision.all import *
 import time
 import cv2
+import numpy as np
 from utils.grabscreen import grab_screen
 
 def label_func(x): return x.parent.name
@@ -10,7 +11,6 @@ def run():
     fnames = get_image_files(path)
     print(f"Total Images:{len(fnames)}")
 
-
     dls = ImageDataLoaders.from_path_func(path, fnames, label_func,bs=40, num_workers=0)
     learn = cnn_learner(dls, resnet18, metrics=error_rate)
     print("Loaded")
@@ -18,19 +18,17 @@ def run():
 
     learn.export()
 
-    # start_time = time.time()
-    # test = learn.predict('g1-j5.png')
-    # print("--- %s seconds ---" % (time.time() - start_time))
-    # print(test)
     start_time = time.time()
     image = grab_screen(region=(50, 100, 799, 449))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.Canny(image, threshold1=200, threshold2=300)
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)  # Convert back to 3 channel image
     image = cv2.resize(image, (224, 224))
+    image = image / 255.0  # Normalize pixel values
+    image = image.astype(np.float32)  # Convert image data type to float32
     test = learn.predict(image)
     print("--- %s seconds ---" % (time.time() - start_time))
     print(test)
-
 
 if __name__ == '__main__':
     run()
